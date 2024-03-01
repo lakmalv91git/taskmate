1b6d6084df676c718839b1fc3fd4200d5fa3c3d3
lakmalv91git <lakmalv91@gmail.com>
2024-03-02 07:32:26 +1300

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import { Header } from "./components/Header";
+import { TaskList } from "./components/TaskList";
+import "./App.css";
+
+export default function App() {
+
+    const concept = "Prop Drilling";
+
+    return (
+        <div className="App">
+            <Header />
+            <TaskList concept={concept} />
+        </div>
+    );
+
+
+
+
+    // const [count, setCount] = useState(0);
+
+    // function handleAdd() {
+    //     setCount(count + 1)
+    // }
+
+    // function handleSub() {
+    //     setCount(count - 1);
+    // }
+    // function handleReset() {
+    //     setCount(0);
+    // }
+    // return (
+    //     <div className="App">
+    //         <div className="box">
+    //             <p>{count}</p>
+    //             <button onClick={handleAdd} className="add">ADD</button>
+    //             <button onClick={handleSub} className="sub">SUB</button>
+    //             <button onClick={handleReset} className="reset">RESET</button>
+    //         </div>
+    //     </div>
+
+
+    // )
+
+
+
+
+}
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import LOGO from "../assets/logo192.png"
+import "../App.css";
+export const Header = () => {
+  return (
+    <header>
+        <img src={LOGO} alt="" />
+        <a href="/">Home</a>
+    </header>
+  )
+}
